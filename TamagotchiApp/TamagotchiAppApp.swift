a88840ccecc334b001f318cb2c5f7ef3176e743d
Luke Vereker <vereker.l@etoncollege.org.uk>
2023-01-14 10:50:08 +0000

    Initial Commit

---
+++ TamagotchiApp/TamagotchiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiAppApp.swift
+//  TamagotchiApp
+//
+//  Created by Vereker, Luke (PRKB) on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
